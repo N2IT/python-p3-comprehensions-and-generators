ade8b5726c3c76c4b4b121b2fae3728a793b5686
Tony Eder <tonyeder11@gmail.com>
2024-02-02 12:03:04 -0800

    list comp complete. if statement at end

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
-
+   only_even = [ n for n in num_list if n % 2 == 0 ]
+   return only_even
+   
 def make_exclamation(sentence_list):
     pass
